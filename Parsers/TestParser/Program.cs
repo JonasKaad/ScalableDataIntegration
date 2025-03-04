53ed73d7c2ecc36814a1e11bef9397955aea17db
Jonas Kaad <jonaskaad@gmail.com>
2025-03-04 11:37:19 +0100

    Removed unused health check

--- Parsers/TestParser/Program.cs
+++ Parsers/TestParser/Program.cs
@@ -5,25 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddHealthChecks();
-
-builder.WebHost.ConfigureKestrel(o =>
-{
-    o.ListenAnyIP(8084, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http1;
-    });
-    o.ListenAnyIP(8080, listenOptions =>
-    {
-        listenOptions.Protocols = HttpProtocols.Http2;
-    });
-});
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<TestParserService>();
-app.MapHealthChecks("/health").RequireHost("*:8084");
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
