42473d18adcf247fcb8d78a8d4ffce259505b400
Jonas Kaad <jonaskaad@gmail.com>
2025-03-11 15:10:08 +0100

    ParserPanel now opens the new Dialog component

--- SkyPanel/Components/Features/ParserPanel.razor.cs
+++ SkyPanel/Components/Features/ParserPanel.razor.cs
@@ -6,55 +6,10 @@ namespace SkyPanel.Components.Features;
 
 public partial class ParserPanel : ComponentBase
 {
-    private const string DefaultDragClass = "relative rounded-lg border-2 border-dashed pa-4 mt-4 mud-width-full mud-height-full";
-    private string _dragClass = DefaultDragClass;
-    private readonly List<string> _fileNames = new();
-    private MudFileUpload<IReadOnlyList<IBrowserFile>>? _fileUpload;
-    
-    private async Task ClearAsync()
+    private Task OpenDialogAsync()
     {
-        await (_fileUpload?.ClearAsync() ?? Task.CompletedTask);
-        _fileNames.Clear();
-        StateHasChanged();
-        ClearDragClass();
-    }
-    
-    private void RemoveFile(string fileName)
-    {
-        _fileNames.Remove(fileName);
-        StateHasChanged();
-        ClearDragClass();
-    }
-    
-    private Task OpenFilePickerAsync()
-        => _fileUpload?.OpenFilePickerAsync() ?? Task.CompletedTask;
-    
-    private void OnInputFileChanged(InputFileChangeEventArgs e)
-    {
-        ClearDragClass();
-        var files = e.GetMultipleFiles();
-        foreach (var file in files)
-        {
-            _fileNames.Add(file.Name);
-            FilePopUp(file.Name);
-        }
-    }
-    
-    private void Upload()
-    {
-        // Upload the files here
-        Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
-        Snackbar.Add("Uploaded your files!");
-    }
-    
-    private void SetDragClass()
-    {
-        _dragClass = $"{DefaultDragClass} mud-border-warning";
-    }
-    private void FilePopUp(string file) => Snackbar.Add($"Added file: {file}", Severity.Success );
+        var options = new DialogOptions { CloseOnEscapeKey = true };
 
-    private void ClearDragClass()
-    {
-        _dragClass = DefaultDragClass;
+        return DialogService.ShowAsync<FileDialogParser>("Simple Dialog", options);
     }
 }
