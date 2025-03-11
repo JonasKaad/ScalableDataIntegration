94dd7a971d07a5f665de7ccf16c99bb20b902cd9
Jonas Kaad <jonaskaad@gmail.com>
2025-03-11 15:12:44 +0100

    File Upload now opens as a dialog

--- SkyPanel/Components/Features/FileDialogParser.razor.cs
+++ SkyPanel/Components/Features/FileDialogParser.razor.cs
@@ -1,7 +1,60 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
 
 namespace SkyPanel.Components.Features;
 
 public partial class FileDialogParser : ComponentBase
 {
+    private const string DefaultDragClass = "relative rounded-lg border-2 border-dashed pa-4 mt-4 mud-width-full mud-height-full";
+    private string _dragClass = DefaultDragClass;
+    private readonly List<string> _fileNames = new();
+    private MudFileUpload<IReadOnlyList<IBrowserFile>>? _fileUpload;
+    
+    private async Task ClearAsync()
+    {
+        await (_fileUpload?.ClearAsync() ?? Task.CompletedTask);
+        _fileNames.Clear();
+        StateHasChanged();
+        ClearDragClass();
+    }
+    
+    private void RemoveFile(string fileName)
+    {
+        _fileNames.Remove(fileName);
+        StateHasChanged();
+        ClearDragClass();
+    }
+    
+    private Task OpenFilePickerAsync()
+        => _fileUpload?.OpenFilePickerAsync() ?? Task.CompletedTask;
+    
+    private void OnInputFileChanged(InputFileChangeEventArgs e)
+    {
+        ClearDragClass();
+        var files = e.GetMultipleFiles();
+        foreach (var file in files)
+        {
+            _fileNames.Add(file.Name);
+            FilePopUp(file.Name);
+        }
+    }
+    
+    private void Upload()
+    {
+        // Upload the files here
+        Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
+        Snackbar.Add("Uploaded your files!");
+    }
+    
+    private void SetDragClass()
+    {
+        _dragClass = $"{DefaultDragClass} mud-border-warning";
+    }
+    private void FilePopUp(string file) => Snackbar.Add($"Added file: {file}", Severity.Success );
+
+    private void ClearDragClass()
+    {
+        _dragClass = DefaultDragClass;
+    }
 }
