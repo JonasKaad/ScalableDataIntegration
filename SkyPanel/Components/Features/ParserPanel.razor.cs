398d461f94e792ad290c797fc7efa555f54ddc50
Jonas Kaad <jonaskaad@gmail.com>
2025-03-11 13:44:28 +0100

    Updated file uploading to drag and drop

--- SkyPanel/Components/Features/ParserPanel.razor.cs
+++ SkyPanel/Components/Features/ParserPanel.razor.cs
@@ -1,25 +1,60 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
 
 namespace SkyPanel.Components.Features;
 
 public partial class ParserPanel : ComponentBase
 {
-    IList<IBrowserFile> _files = new List<IBrowserFile>();
-    private void UploadFiles(IReadOnlyList<IBrowserFile>? files)
+    private const string DefaultDragClass = "relative rounded-lg border-2 border-dashed pa-4 mt-4 mud-width-full mud-height-full";
+    private string _dragClass = DefaultDragClass;
+    private readonly List<string> _fileNames = new();
+    private MudFileUpload<IReadOnlyList<IBrowserFile>>? _fileUpload;
+
+    private async Task ClearAsync()
     {
-        //TODO upload the files to the server / handle them
-        if (files != null) {
-            foreach (var file in files)
-            {
-                _files.Add(file);
-            }
+        await (_fileUpload?.ClearAsync() ?? Task.CompletedTask);
+        _fileNames.Clear();
+        StateHasChanged();
+        ClearDragClass();
+    }
+    
+    private async Task RemoveFileAsync(string fileName)
+    {
+        await (_fileUpload?.ClearAsync() ?? Task.CompletedTask);
+        _fileNames.Remove(fileName);
+        StateHasChanged();
+        ClearDragClass();
+    }
+
+    private Task OpenFilePickerAsync()
+        => _fileUpload?.OpenFilePickerAsync() ?? Task.CompletedTask;
+
+    private void OnInputFileChanged(InputFileChangeEventArgs e)
+    {
+        ClearDragClass();
+        var files = e.GetMultipleFiles();
+        foreach (var file in files)
+        {
+            _fileNames.Add(file.Name);
         }
     }
 
-    private void UploadFiles2(IBrowserFile file)
+    private void Upload()
     {
-        //TODO upload the file to the server / handle it
-        _files.Add(file);
+        // Upload the files here
+        Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
+        Snackbar.Add("TODO: Upload your files!");
+    }
+
+    private void SetDragClass()
+    {
+        _dragClass = $"{DefaultDragClass} mud-border-warning";
+    }
+
+
+    private void ClearDragClass()
+    {
+        _dragClass = DefaultDragClass;
     }
 }
