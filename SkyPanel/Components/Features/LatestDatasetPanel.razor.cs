7441574509c587111de5307d050debecd52958a0
Jonas Kaad <jonaskaad@gmail.com>
2025-03-12 12:00:02 +0100

    Created basic table layout based on fake data

--- SkyPanel/Components/Features/LatestDatasetPanel.razor.cs
+++ SkyPanel/Components/Features/LatestDatasetPanel.razor.cs
@@ -4,4 +4,25 @@ namespace SkyPanel.Components.Features;
 
 public partial class LatestDatasetPanel : ComponentBase
 {
+    private WeatherForecast[]? forecasts;
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Simulate asynchronous loading to demonstrate a loading indicator
+        await Task.Delay(500);
+
+        var startDate = DateOnly.FromDateTime(DateTime.Now);
+        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+        forecasts = Enumerable.Range(1, 30).Select(index => new WeatherForecast
+        {
+            Date = startDate.AddDays(index),
+            Summary = summaries[Random.Shared.Next(summaries.Length)]
+        }).ToArray();
+    }
+
+    private class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+        public string? Summary { get; set; }
+    }
 }
