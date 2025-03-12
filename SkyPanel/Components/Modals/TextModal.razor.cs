640696d1fac9696e5b596a2c58ebde4513799a1f
Jonas Kaad <jonaskaad@gmail.com>
2025-03-12 09:46:34 +0100

    Created generic modal for using within the whole dashboard

--- SkyPanel/Components/Modals/TextModal.razor.cs
+++ SkyPanel/Components/Modals/TextModal.razor.cs
@@ -1,7 +1,22 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace SkyPanel.Components.Modals;
 
 public partial class TextModal : ComponentBase
 {
+    [Parameter]
+    public string ContentText { get; set; } = string.Empty;
+    
+    [Parameter]
+    public string ConfirmationButtonText { get; set; } = "Ok";
+    
+    [Parameter]
+    public Color Color { get; set; } = Color.Success;
+    
+    
+    [CascadingParameter]
+    private IMudDialogInstance MudDialog { get; set; }
+    private void DialogSubmit() => MudDialog.Close(DialogResult.Ok(true));
+    private void DialogCancel() => MudDialog.Cancel();
 }
