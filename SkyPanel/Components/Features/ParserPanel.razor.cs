3b279963f4201249ddc3684d5258de866e266419
Jonas Kaad <jonaskaad@gmail.com>
2025-03-12 09:47:54 +0100

    Added new method for opening the generic modal from parserpanel

--- SkyPanel/Components/Features/ParserPanel.razor.cs
+++ SkyPanel/Components/Features/ParserPanel.razor.cs
@@ -8,9 +8,24 @@ namespace SkyPanel.Components.Features;
 public partial class ParserPanel : ComponentBase
 {
     private Task OpenDialogAsync()
+    private Task OpenFileDialogAsync()
     {
         var options = new DialogOptions { CloseOnEscapeKey = true, MaxWidth = MaxWidth.Small, FullWidth = true };
 
         return DialogService.ShowAsync<FileDialogParser>("Upload Dataset", options);
+    }
+
+    private Task OpenReparseDialogAsync()
+    {
+        var parameters = new DialogParameters<TextModal>
+        {
+            { x => x.ContentText, "Do you want to fetch and reparse the latest data for: \n" + ParserName },
+            { x => x.ConfirmationButtonText, "Ok" },
+            { x => x.Color, Color.Success }
+        };
+        
+        var options = new DialogOptions { CloseOnEscapeKey = true, MaxWidth = MaxWidth.ExtraSmall, FullWidth = true };
+        
+        return DialogService.ShowAsync<TextModal>("Fetch and parse latest dataset", parameters, options);
     }
 }
