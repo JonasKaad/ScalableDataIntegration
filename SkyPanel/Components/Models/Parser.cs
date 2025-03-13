5c4bd8c60d979607dea5f9776eb430fb02b42e40
Jonas Kaad <jonaskaad@gmail.com>
2025-03-13 14:32:50 +0100

    Implemented the basic Parser model with variables

--- SkyPanel/Components/Models/Parser.cs
+++ SkyPanel/Components/Models/Parser.cs
@@ -1,6 +1,24 @@
+using MudBlazor;
+
 namespace SkyPanel.Components.Models;
 
 public class Parser
 {
+    public string Name { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string Url { get; set; }
+    public string Protocol { get; set; }
+    public int Polling { get; set; }
     
+    
+    public Parser(string name, string username, string password, string url, string protocol, int polling)
+    {
+        Name = name;
+        Username = username;
+        Password = password;
+        Url = url;
+        Protocol = protocol;
+        Polling = polling;
+    }
 }
