0922787adca24edd2acea02cc2067b61811a2abd
Jonas Kaad <jonaskaad@gmail.com>
2025-03-13 12:07:58 +0100

    Removed unused boilerplate from Dashboard

--- SkyPanel/Components/Dashboard.razor.cs
+++ SkyPanel/Components/Dashboard.razor.cs
@@ -4,16 +4,4 @@ namespace SkyPanel.Components;
 
 public partial class Dashboard : ComponentBase
 {
-    private string _name = "World";
-
-
-    public void SetName(string Name)
-    {
-        _name = Name;
-    }
-
-    public void PrintName()
-    {
-        Console.WriteLine(_name);
-    }
 }
