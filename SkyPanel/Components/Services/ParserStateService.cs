535e77adef4814f183fe974fb2723589c58f1a1c
Jonas Kaad <jonaskaad@gmail.com>
2025-03-13 16:17:21 +0100

    Removed username and password from parserstateservice

--- SkyPanel/Components/Services/ParserStateService.cs
+++ SkyPanel/Components/Services/ParserStateService.cs
@@ -2,15 +2,10 @@ namespace SkyPanel.Components.Services;
 using SkyPanel.Components.Models;
 
 public class ParserStateService
-{
-    private IDictionary<string, IDictionary<string, string>> _secretDictionary = new Dictionary<string, IDictionary<string, string>>();
-
+{ 
     private Parser? _parser;
-    
-    public Parser Parser => _parser;
+
     public string ParserName => _parser?.Name ?? string.Empty;
-    public string Username => _parser?.Username ?? string.Empty;
-    public string Password => _parser?.Password ?? string.Empty;
     
     public string Url => _parser?.Url ?? string.Empty;
     
@@ -19,12 +14,6 @@ public class ParserStateService
     public string Protocol => _parser?.Protocol ?? string.Empty;
 
     public event Action? OnChange;
-    
-    
-    public void InitializeDictionary(IDictionary<string, IDictionary<string, string>> secretDictionary)
-    {
-        _secretDictionary = secretDictionary;
-    }
     
     public void SetParser(Parser? parser)
     {
@@ -36,7 +25,7 @@ public class ParserStateService
     
     public Parser[] TestParsers =
     [
-        new("NatTrackParser", "jonas", "1234", "https://www.google.com", "Http", 24),
-        new("BufrParser", "victor", "4321", "ftp://www.test.com", "Ftp", 37)
+        new("NatTrackParser", "https://www.google.com", "Http", 24),
+        new("BufrParser",  "ftp://www.test.com", "Ftp", 37)
     ];
 }
