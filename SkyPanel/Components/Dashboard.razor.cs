2f99be5662b58cf77b5734592656bb378837e9ee
Jonas Kaad <jonaskaad@gmail.com>
2025-03-17 11:06:21 +0100

    Opacity of protocol panel changes based on if parser is selected

--- SkyPanel/Components/Dashboard.razor.cs
+++ SkyPanel/Components/Dashboard.razor.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Components;
 
 namespace SkyPanel.Components;
+using SkyPanel.Components.Services;
 
 public partial class Dashboard : ComponentBase
 {
+    [Inject] private ParserStateService ParserState { get; set; } = default!;
 }
