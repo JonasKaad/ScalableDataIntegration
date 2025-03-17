b01ed53ebacaf8bcd52ea2e1416f7d7ce96004b9
Jonas Kaad <jonaskaad@gmail.com>
2025-03-17 09:50:02 +0100

    Simple credentials dialog that shows username and password

--- SkyPanel/Components/Dialogs/CredentialsDialog.razor.cs
+++ SkyPanel/Components/Dialogs/CredentialsDialog.razor.cs
@@ -1,7 +1,23 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace SkyPanel.Components.Dialogs;
 
 public partial class CredentialsDialog : ComponentBase
 {
+    [Parameter]
+    public  string Username { get; set; }= string.Empty;
+    
+    [Parameter]
+    public string Password { get; set; } = string.Empty;
+    
+    
+    [CascadingParameter]
+    private IMudDialogInstance? MudDialog { get; set; }
+    private void DialogSubmit()
+    {
+        MudDialog?.Close(DialogResult.Ok(true));
+    }
+
+    private void DialogCancel() => MudDialog?.Cancel();
 }
