c1d6431640a880974505cb556dbc0a593ab0a6b1
Jonas Kaad <jonaskaad@gmail.com>
2025-03-17 14:20:27 +0100

    Setup loading data from db in Latest Dataset

--- SkyPanel/Components/Features/LatestDatasetPanel.razor.cs
+++ SkyPanel/Components/Features/LatestDatasetPanel.razor.cs
@@ -1,28 +1,26 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using SkyPanel.Components.Models;
+using SkyPanel.Components.Services;
 
 namespace SkyPanel.Components.Features;
 
 public partial class LatestDatasetPanel : ComponentBase
 {
-    private WeatherForecast[]? _forecasts;
+    private Dataset[]? _datasets;
+    [Inject] private StatisticsDatabaseService Db { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
         // Simulate asynchronous loading to demonstrate a loading indicator
-        await Task.Delay(500);
+        //wait Task.Delay(500);
+        
+        
 
-        var startDate = DateOnly.FromDateTime(DateTime.Now);
-        var summaries = new[] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-        _forecasts = Enumerable.Range(1, 30).Select(index => new WeatherForecast
-        {
-            Date = startDate.AddDays(index),
-            Summary = summaries[Random.Shared.Next(summaries.Length)]
-        }).ToArray();
-    }
-
-    private class WeatherForecast
-    {
-        public DateOnly Date { get; set; }
-        public string? Summary { get; set; }
+        Console.WriteLine("Querying for statistics");
+        
+        _datasets = await Db.Datasets
+            .OrderBy(b => b.Date)
+            .ToArrayAsync();
     }
 }
