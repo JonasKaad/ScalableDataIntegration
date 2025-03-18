93bbd943703d06ed162c834d632188f62d519556
Jonas Kaad <jonaskaad@gmail.com>
2025-03-18 09:47:50 +0100

    BlobManagerService can retrieve all containers and blob paths

--- SkyPanel/Components/Services/BlobManagerService.cs
+++ SkyPanel/Components/Services/BlobManagerService.cs
@@ -1,6 +1,77 @@
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using DotNetEnv;
+
 namespace SkyPanel.Components.Services;
 
 public class BlobManagerService
 {
+    private BlobServiceClient ServiceClient { get; }
+    private IList<BlobContainerItem> blobContainers;
+
+    private Dictionary<BlobContainerItem, List<BlobItem>> blobs = new Dictionary<BlobContainerItem, List<BlobItem>>();
+
+    public BlobManagerService(string connectionString) 
+    {
+        ServiceClient = new BlobServiceClient(connectionString);
+        blobContainers = new List<BlobContainerItem>();
+        blobs = new Dictionary<BlobContainerItem, List<BlobItem>>();
+    }
+
+
+    public void GetContainers()
+    {
+        blobContainers.Clear();
+        blobs.Clear();
+        
+        foreach (var container in ServiceClient.GetBlobContainers())
+        {
+            blobContainers.Add(container);
+        }
+    } 
+    public List<string> GetContainerNames()
+    {
+        var containerNames = new List<string>();        
+        
+        foreach (var container in ServiceClient.GetBlobContainers())
+        {
+            containerNames.Add(container.Name);
+        }
+        
+        return containerNames;
+    }
+    public void GetAllBlobItems()
+    {
+        foreach (var container in blobContainers)
+        {
+            Console.WriteLine(container.Name);
+            var containerClient = ServiceClient.GetBlobContainerClient(container.Name);
+            var blobItems = new List<BlobItem>();
+            foreach (var blob in containerClient.GetBlobs())
+            {
+                blobItems.Add(blob);
+            }
+            blobs.Add(container, blobItems);
+        }
+    }
+
+    public void PrintDictionary()
+    {
+        foreach (var (container, blob) in blobs)
+        {
+            foreach (var blobItem in blob)
+            {
+                Console.WriteLine(container.Name + " " + blobItem.Name);
+            }
+        }
+    }
     
+    public void GetBlobItems(string containerName)
+    {
+        var containerClient = ServiceClient.GetBlobContainerClient(containerName);
+        foreach (var blobItem in containerClient.GetBlobs())
+        {
+            Console.WriteLine("\t" + blobItem.Name);
+        }
+    }
 }
