993381f3c24f3a2546e7199b70542f395b9ce462
Jonas Kaad <jonaskaad@gmail.com>
2025-03-18 11:12:49 +0100

    BlobDataItem now contains file paths to raw and parsed

--- SkyPanel/Components/Models/BlobDataItem.cs
+++ SkyPanel/Components/Models/BlobDataItem.cs
@@ -2,14 +2,22 @@ namespace SkyPanel.Components.Models;
 
 public class BlobDataItem
 {
-    private String Parser { get; set; }
-    private DateTime Date { get; set; }
+    public string Parser { get; set; }
+    public DateTime Date { get; set; }
     
-    public BlobDataItem(String parser, DateTime date)
+    public string? RawPath { get; set; }
+    public string? ParsedPath { get; set; }
+    
+    public BlobDataItem(String parser, DateTime date, string? rawPath = null, string? parsedPath = null)
     {
         Parser = parser;
         Date = date;
+        RawPath = rawPath;
+        ParsedPath = parsedPath;
     }
-    
-    
+
+    public override string ToString()
+    {
+        return $"{Parser} - {Date} - {RawPath} - {ParsedPath}";
+    }
 }
