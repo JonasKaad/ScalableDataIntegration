37f61b386aa62625f9a55061a2106f4d4ac76a65
Jonas Kaad <jonaskaad@gmail.com>
2025-03-18 09:48:24 +0100

    Removed TestParsers from parserstateservice

--- SkyPanel/Components/Services/ParserStateService.cs
+++ SkyPanel/Components/Services/ParserStateService.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Components;
+
 namespace SkyPanel.Components.Services;
 using SkyPanel.Components.Models;
 
 public class ParserStateService
 { 
     private Parser? _parser;
-
     public string ParserName => _parser?.Name ?? string.Empty;
     
     public string Url => _parser?.Url ?? string.Empty;
@@ -24,9 +25,4 @@ public class ParserStateService
 
     public bool ParserIsNotSelected() => _parser == null;
     
-    public Parser[] TestParsers =
-    [
-        new("NatTrackParser", "https://www.google.com", "Http", 24),
-        new("BufrParser",  "ftp://www.test.com", "Ftp", 37)
-    ];
 }
