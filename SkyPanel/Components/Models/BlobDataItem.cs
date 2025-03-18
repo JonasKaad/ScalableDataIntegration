affe69e53c44fbb7167fb4bf9c1103dc22bfece3
Jonas Kaad <jonaskaad@gmail.com>
2025-03-18 09:50:49 +0100

    BlobDataItem to hold data from Azure Blob storage

--- SkyPanel/Components/Models/BlobDataItem.cs
+++ SkyPanel/Components/Models/BlobDataItem.cs
@@ -2,5 +2,14 @@ namespace SkyPanel.Components.Models;
 
 public class BlobDataItem
 {
+    private String Parser { get; set; }
+    private DateTime Date { get; set; }
+    
+    public BlobDataItem(String parser, DateTime date)
+    {
+        Parser = parser;
+        Date = date;
+    }
+    
     
 }
