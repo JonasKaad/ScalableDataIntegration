7aaa9c68fcdec00fe099b3d6122660b00abbd311
Jonas Kaad <jonaskaad@gmail.com>
2025-03-20 14:27:22 +0100

    Created Dialog for blob delete confirmation

--- SkyPanel/Components/Dialogs/BlobDeleteDialog.razor.cs
+++ SkyPanel/Components/Dialogs/BlobDeleteDialog.razor.cs
@@ -1,7 +1,27 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using SkyPanel.Components.Services;
 
 namespace SkyPanel.Components.Dialogs;
 
 public partial class BlobDeleteDialog : ComponentBase
 {
+    [Parameter]
+    public  string RawDataset { get; set; }= string.Empty;
+    
+    [Parameter]
+    public string ParsedDataset { get; set; } = string.Empty;
+    
+    [Parameter, Required]
+    public required string Parser { get; set; }
+    
+    [Inject] private BlobManagerService BlobService { get; set; } = default!;
+
+    [CascadingParameter] IMudDialogInstance? MudDialog { get; set; }
+    
+    private void DialogSubmit() => MudDialog?.Close(DialogResult.Ok("delete"));
+
+    private void DialogCancel() => MudDialog?.Cancel();
+    
 }
