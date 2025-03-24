6470c890bece42d80178a8e841a1e8e4e865a90f
Jonas Kaad <jonaskaad@gmail.com>
2025-03-24 14:02:20 +0100

    Created simple http request to /downloaders

--- SkyPanel/Components/Services/OrchestratorClientService.cs
+++ SkyPanel/Components/Services/OrchestratorClientService.cs
@@ -1,6 +1,12 @@
 namespace SkyPanel.Components.Services;
 
-public class OrchestratorClientService
+public sealed class OrchestratorClientService(IHttpClientFactory httpClientFactory, string baseUrl)
 {
-    
+    public async Task<List<string>> GetDownloaders()
+    {
+        var client = httpClientFactory.CreateClient();
+        var response = await client.GetAsync($"{baseUrl}downloaders");
+        var elements = await response.Content.ReadFromJsonAsync<List<string>>();
+        return elements;
+    }
 }
