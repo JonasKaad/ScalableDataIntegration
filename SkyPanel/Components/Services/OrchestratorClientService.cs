1dbee2bf5f024b33e3ed2ae5874531e885cbe118
Jonas Kaad <jonaskaad@gmail.com>
2025-03-24 15:53:36 +0100

    Updated method for calling /downloaders endpoint

--- SkyPanel/Components/Services/OrchestratorClientService.cs
+++ SkyPanel/Components/Services/OrchestratorClientService.cs
@@ -1,3 +1,26 @@
+using System.Text.Json;
+using SkyPanel.Components.Models;
+
+namespace SkyPanel.Components.Services;
+
+public sealed class OrchestratorClientService(IHttpClientFactory httpClientFactory, string baseUrl)
+{
+    public async Task<IEnumerable<string>> GetDownloaders()
+    {
+        var client = httpClientFactory.CreateClient();
+        try
+        {
+            var response = await client.GetAsync($"{baseUrl}/downloaders");
+            var elements = await response.Content.ReadFromJsonAsync<IEnumerable<string>>();
+            return elements ?? [];
+        } 
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        return [];
+    }
+
 namespace SkyPanel.Components.Services;
 
 public sealed class OrchestratorClientService(IHttpClientFactory httpClientFactory, string baseUrl)
