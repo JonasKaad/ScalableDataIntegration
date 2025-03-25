e060a4bf5524ec7012f6490316565929dea119de
Jonas Kaad <jonaskaad@gmail.com>
2025-03-25 09:41:40 +0100

    Set variables for update params

--- SkyPanel/Components/Dialogs/UpdateDialog.razor.cs
+++ SkyPanel/Components/Dialogs/UpdateDialog.razor.cs
@@ -5,22 +5,24 @@ using SkyPanel.Components.Services;
 
 namespace SkyPanel.Components.Dialogs;
 
-public partial class BlobDeleteDialog : ComponentBase
+public partial class UpdateDialog : ComponentBase
 {
-    [Parameter]
-    public  string RawDataset { get; set; }= string.Empty;
-    
-    [Parameter]
-    public string ParsedDataset { get; set; } = string.Empty;
-    
     [Parameter, Required]
     public required string Parser { get; set; }
+    [Parameter]
+    public  string Url { get; set; }= string.Empty;
+    [Parameter]
+    public string BackupUrl { get; set; } = string.Empty;
+    [Parameter]
+    public string SecretName { get; set; } = string.Empty;
+    [Parameter]
+    public string PollingRate { get; set; } = string.Empty;
     
-    [Inject] private BlobManagerService BlobService { get; set; } = default!;
-
+    [Inject] private ParserStateService ParserState { get; set; } = null!;
+    
     [CascadingParameter] IMudDialogInstance? MudDialog { get; set; }
     
-    private void DialogSubmit() => MudDialog?.Close(DialogResult.Ok("delete"));
+    private void DialogSubmit() => MudDialog?.Close(DialogResult.Ok("update"));
 
     private void DialogCancel() => MudDialog?.Cancel();
     
