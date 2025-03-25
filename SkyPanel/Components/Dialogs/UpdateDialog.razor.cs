cf90c80ba0718500188289092ed060f71e750317
Jonas Kaad <jonaskaad@gmail.com>
2025-03-25 10:44:46 +0100

    Implemented method for comparing two strings and finding diff

--- SkyPanel/Components/Dialogs/UpdateDialog.razor.cs
+++ SkyPanel/Components/Dialogs/UpdateDialog.razor.cs
@@ -26,4 +26,99 @@ public partial class UpdateDialog : ComponentBase
 
     private void DialogCancel() => MudDialog?.Cancel();
     
+    // Inspired by @https://github.com/google/diff-match-patch/blob/master/csharp/DiffMatchPatch.cs
+    private string HighlightAddedContent(string? oldValue, string? newValue)
+    {
+        if (string.IsNullOrEmpty(newValue))
+        {
+            return string.Empty;
+        }
+    
+        // If old value is empty, highlight the entire new value as added
+        if (string.IsNullOrEmpty(oldValue))
+        {
+            return $"<span style=\"background-color: #d0f0c0; font-weight: bold;\">{newValue}</span>";
+        }
+
+        if (oldValue == newValue)
+        {
+            return newValue;
+        }
+
+        // Simple diff logic to find common prefixes and suffixes
+        int prefixLength = 0;
+        int minLength = Math.Min(oldValue.Length, newValue.Length);
+
+        // Find common prefix
+        while (prefixLength < minLength && oldValue[prefixLength] == newValue[prefixLength])
+        {
+            prefixLength++;
+        }
+
+        // Find common suffix
+        int oldIndex = oldValue.Length - 1;
+        int newIndex = newValue.Length - 1;
+        int suffixLength = 0;
+
+        while (suffixLength < minLength - prefixLength &&
+               oldIndex >= prefixLength &&
+               newIndex >= prefixLength &&
+               oldValue[oldIndex] == newValue[newIndex])
+        {
+            oldIndex--;
+            newIndex--;
+            suffixLength++;
+        }
+
+        string prefix = newValue.Substring(0, prefixLength);
+        string changed = newValue.Substring(prefixLength, newValue.Length - prefixLength - suffixLength);
+        string suffix = newValue.Substring(newValue.Length - suffixLength);
+
+        return $"{prefix}<span style=\"background-color: #d0f0c0; font-weight: bold;\">{changed}</span>{suffix}";
+    }
+
+    // Inspired by @https://github.com/google/diff-match-patch/blob/master/csharp/DiffMatchPatch.cs
+    private string HighlightRemovedContent(string? oldValue, string? newValue)
+    {
+        if (string.IsNullOrEmpty(oldValue) || string.IsNullOrEmpty(newValue))
+        {
+            return oldValue ?? string.Empty;
+        }
+
+        if (oldValue == newValue)
+        {
+            return oldValue;
+        }
+
+        // Simple diff logic to find common prefixes and suffixes
+        int prefixLength = 0;
+        int minLength = Math.Min(oldValue.Length, newValue.Length);
+
+        // Find common prefix
+        while (prefixLength < minLength && oldValue[prefixLength] == newValue[prefixLength])
+        {
+            prefixLength++;
+        }
+
+        // Find common suffix
+        int oldIndex = oldValue.Length - 1;
+        int newIndex = newValue.Length - 1;
+        int suffixLength = 0;
+
+        while (suffixLength < minLength - prefixLength && 
+               oldIndex >= prefixLength && 
+               newIndex >= prefixLength && 
+               oldValue[oldIndex] == newValue[newIndex])
+        {
+            oldIndex--;
+            newIndex--;
+            suffixLength++;
+        }
+
+        string prefix = oldValue.Substring(0, prefixLength);
+        string changed = oldValue.Substring(prefixLength, oldValue.Length - prefixLength - suffixLength);
+        string suffix = oldValue.Substring(oldValue.Length - suffixLength);
+
+        return $"{prefix}<span style=\"background-color: #ffcccc; font-weight: bold;\">{changed}</span>{suffix}";
+    }
 }
