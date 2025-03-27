b598f04cae6746c5acc68d9ce74e861890143874
Jonas Kaad <jonaskaad@gmail.com>
2025-03-27 15:46:22 +0100

    Fixed bug with persisting parsername, again

--- SkyPanel/Components/Features/ParserPanel.razor.cs
+++ SkyPanel/Components/Features/ParserPanel.razor.cs
@@ -16,12 +16,12 @@ public partial class ParserPanel : ComponentBase
     
     public string Parser
     {
-        get => ParserState.ParserName;
+        get => string.IsNullOrEmpty(ParserState.ParserName) ? _selectedParser : ParserState.ParserName;
         set
         {
+            _selectedParser = value;
             _ = InvokeAsync(async () => 
             {
-                _selectedParser = value;
                 await GetDownloaderConfiguration(value);
                 StateHasChanged();
             });
@@ -38,6 +38,7 @@ public partial class ParserPanel : ComponentBase
         // Get the full parser configuration and store it
         var parserConfig = await OrchestratorClient.GetDownloaderConfiguration(parserName);
         ParserState.SetParser(parserConfig);
+        StateHasChanged();
     }
     
     protected override void OnInitialized()
