c6e10ef3fa00882bcb2c449c17ff00e996046f6f
Jonas Kaad <jonaskaad@gmail.com>
2025-03-31 13:14:56 +0200

    AuthService setting up the parameters for auth0

--- DownloadOrchestrator/Services/AuthService.cs
+++ DownloadOrchestrator/Services/AuthService.cs
@@ -1,6 +1,26 @@
+using DownloadOrchestrator.Models;
+
 namespace DownloadOrchestrator.Services;
 
 public class AuthService
 {
+    private AuthModel _authModel;
     
+    public AuthService(string domain, string clientId, string clientSecret)
+    {
+        _authModel = new AuthModel(domain, clientId, clientSecret);
+    }
+
+    public string GetDomain()
+    {
+        return _authModel.Domain;
+    }
+    public string GetClientId()
+    {
+        return _authModel.ClientId;
+    }
+    public string GetClientSecret()
+    {
+        return _authModel.ClientSecret;
+    }
 }
