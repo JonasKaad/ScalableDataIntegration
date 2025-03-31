7a5d0cc784c440d396cb3615a051834818a1f4de
Jonas Kaad <jonaskaad@gmail.com>
2025-03-31 13:14:39 +0200

    Created AuthModel for domain, clientid & secret

--- DownloadOrchestrator/Models/AuthModel.cs
+++ DownloadOrchestrator/Models/AuthModel.cs
@@ -1,6 +1,8 @@
 namespace DownloadOrchestrator.Models;
 
-public class AuthModel
+public class AuthModel(string domain, string clientId, string clientSecret)
 {
-    
+    public string Domain { get; } = domain;
+    public string ClientId { get; } = clientId;
+    public string ClientSecret { get; } = clientSecret;
 }
