ebd7664cacee1dea37c25cd8b03455d185a788ba
Jonas Kaad <jonaskaad@gmail.com>
2025-04-01 12:52:45 +0200

    Created util class for comparing role and parser names, case insensitive

---
+++ SkyPanel/Utils/RoleUtil.cs
@@ -0,0 +1,26 @@
+using System.Security.Claims;
+
+namespace SkyPanel.Utils;
+
+public class RoleUtil
+{
+    /// <summary>
+    /// Utility class for checking user roles and permissions, regardless of string casing.
+    /// </summary>
+    /// <summary>
+    /// Checks if a user has a specific role that matches the given parser name.
+    /// </summary>
+    /// <param name="parserName">The name of the parser/downloader to check against user roles</param>
+    /// <param name="user">The user whose roles will be checked</param>
+    /// <returns>True if the user has a role matching the parser name (case-insensitive), otherwise false</returns>
+    public static bool HasRole(string parserName, ClaimsPrincipal user)
+    {
+        var userRoles = user.Claims
+            .Where(c => c.Type.Equals(ClaimTypes.Role))
+            .Select(c => c.Value)
+            .ToList();
+
+        return userRoles.Any(role => 
+            string.Equals(role, parserName, StringComparison.OrdinalIgnoreCase));
+    }
+}
