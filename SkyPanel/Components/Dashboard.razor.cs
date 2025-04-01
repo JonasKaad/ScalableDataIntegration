7db4baec3af24315dde7b6c660a3d0a654c5cfe9
Jonas Kaad <jonaskaad@gmail.com>
2025-04-01 11:45:09 +0200

    Dashboard components only show if the user has access to any parser

--- SkyPanel/Components/Dashboard.razor.cs
+++ SkyPanel/Components/Dashboard.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace SkyPanel.Components;
 using SkyPanel.Components.Services;
@@ -6,4 +7,29 @@ using SkyPanel.Components.Services;
 public partial class Dashboard : ComponentBase
 {
     [Inject] private ParserStateService ParserState { get; set; } = default!;
+    [Inject] private OrchestratorClientService OrchestratorClient { get; set; } = default!;
+    [CascadingParameter] private Task<AuthenticationState> AuthenticationStateTask { get; set; } = default!;
+    
+    private bool _canAccessParsers = false;
+    protected override async Task OnInitializedAsync()
+    {
+        await CheckParserAccess();
+    }
+    
+    private async Task CheckParserAccess()
+    {
+        var authState = await AuthenticationStateTask;
+        var user = authState.User;
+        
+        // Admin always has access
+        if (user.IsInRole("Admin"))
+        {
+            _canAccessParsers = true;
+            return;
+        }
+        
+        // Check if user has access to any parser
+        var availableParsers = await OrchestratorClient.GetDownloaders();
+        _canAccessParsers = availableParsers.Any(parser => user.IsInRole(parser));
+    }
 }
