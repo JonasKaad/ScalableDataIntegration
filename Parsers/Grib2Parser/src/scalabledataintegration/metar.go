2d8fe0e6648e7613bf36eec1a2eb8bb0802fe07f
Jonas Kaad <jonaskaad@gmail.com>
2025-04-07 17:45:44 +0200

    go metar parser start

---
+++ Parsers/Grib2Parser/src/scalabledataintegration/metar.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/eugecm/gometar/metar/parser"
+)
+
+// https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKCH
+func main() {
+	p := parser.New()
+	report, _ := p.Parse("EKCH 061620Z 32009KT 290V360 9999 OVC082/// 09/M04 Q1025 NOSIG=")
+	fmt.Println(report.Wind)
+}
