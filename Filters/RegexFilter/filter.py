8e13b7482a8a8c5f08ec24be39b66fcc1d8075b6
Victor Andreas Boye <viboy20@student.sdu.dk>
2025-04-07 14:22:18 +0200

    Update filters

--- Filters/RegexFilter/filter.py
+++ Filters/RegexFilter/filter.py
@@ -18,10 +18,13 @@ print(f"Files in {GENCLIENT_PYTHON_PATH}: {os.listdir(GENCLIENT_PYTHON_PATH)}")
 import filter_pb2 # works even though IDE shows error (suppressing the warning)
 # noinspection PyUnresolvedReferences
 import filter_pb2_grpc # works even though IDE shows error (suppressing the warning)
+
+# Import functions from common.py
+COMMON_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../Common/Python"))
+sys.path.insert(0, COMMON_PATH)
+
 # noinspection PyUnresolvedReferences
-import parser_pb2
-# noinspection PyUnresolvedReferences
-import parser_pb2_grpc
+from common import init_filter, get_next_url, read_params, send_to_next_url, get_data
 
 class FilterServicer(filter_pb2_grpc.FilterServicer):
     async def FilterCall(self, request, context):
@@ -41,80 +44,13 @@ class FilterServicer(filter_pb2_grpc.FilterServicer):
 
         (next_url, urls) = get_next_url(request.next_urls.split(";"))
 
-        if next_url:
-            if next_url.startswith("http://"):
-                next_url = next_url[7:]
-            elif next_url.startswith("https://"):
-                next_url = next_url[8:]
+        data_to_send = raw[0]
+        data_to_send += b'magic'
+        data_to_send += bytes(match, encoding='utf-8')
 
-        if ":" not in next_url:
-            next_url = next_url + ":80"
-        try:
-            # Create channel with timeout options
-            options = [
-                ('grpc.enable_retries', 1),
-                ('grpc.keepalive_time_ms', 10000),
-                ('grpc.dns_resolution_timeout_ms', 5000)
-            ]
-            async with grpc.aio.insecure_channel(next_url, options=options) as channel:
-                if len(parameters) > 0:
-                    stub = filter_pb2_grpc.FilterStub(channel)
-                    response = await stub.FilterCall(
-                        filter_pb2.FilterRequest(raw_data=raw_data, parameters=parameters, next_urls=urls))
-                else:
-                    stub = parser_pb2_grpc.ParserStub(channel)
-                    data_to_send = raw[0]
-                    data_to_send += b'magic'
-                    data_to_send += bytes(match, encoding='utf-8')
-                    response = await stub.ParseCall(parser_pb2.ParseRequest(raw_data=data_to_send))
-
-                if response.success:
-                    success = True
-                    msg = "Filter succeeded sending data to next url"
-                else:
-                    success = False
-                    msg = "Filter failed sending data to next url"
-        except Exception as e:
-            print(f"gRPC connection error: {e}")
-            success = False
-            msg = "gRPC connection error"
+        (success, msg) = await send_to_next_url(next_url, data_to_send, parameters, urls)
 
         return filter_pb2.FilterReply(success=success, err_msg=msg)
-
-def get_data(raw_data, format_type):
-    data_list = raw_data.split(b'magic')
-    strings = []
-    raw = []
-    for data in data_list:
-        if data:  # Skip empty data chunks
-            if format_type == "str":
-                try:
-                    #TODO: FIX ME
-                    strings = data.decode('utf-8').split(";")
-                except UnicodeDecodeError:
-                    print(f"Warning: Could not decode part of data as UTF-8")
-                    raw.append(data)  # Keep as bytes
-            else:
-                raw.append(data)
-
-    return strings, raw
-
-
-def read_params(params_list, relevant_string=""):
-    relevant = {}
-    for parameter in params_list:
-        json_acceptable_string = parameter.replace("'", "\"")
-        if relevant_string in json_acceptable_string:
-            relevant = json.loads(json_acceptable_string)
-            params_list.remove(parameter)
-    return relevant, ";".join(params_list)
-
-def get_next_url(urls):
-    if len(urls) == 0:
-        return None
-    url = urls.pop(0)
-    return url, ";".join(urls)
-
 
 async def serve():
     # Create a gRPC server
@@ -135,5 +71,6 @@ async def serve():
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
+    loop.create_task(init_filter(loop))
     asyncio.set_event_loop(loop)
     loop.run_until_complete(serve())
