be3d57fad479a0be87197adce5dd34e2501a0068
Jonas Kaad <jonaskaad@gmail.com>
2025-04-10 15:10:26 +0200

    Created MetarParser for Go

--- Parsers/MetarParser/src/scalabledataintegration/main.go
+++ Parsers/MetarParser/src/scalabledataintegration/main.go
@@ -2,18 +2,70 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/JonasKaad/ScalableDataIntegration/Common/go/server"
 	"github.com/JonasKaad/ScalableDataIntegration/GeneratedClients/go/parser"
+	"github.com/eugecm/gometar/metar"
+	goMetarParser "github.com/eugecm/gometar/metar/parser"
 	"log"
+	"os"
+	"strings"
 )
 
 type MetarParserImpl struct {
 	parser.UnimplementedParserServer
 }
 
-func (m *MetarParserImpl) ParseCall(ctx context.Context, req *parser.ParseRequest) (*parser.ParseResponse, error) {
+func (m *MetarParserImpl) ParseCall(_ context.Context, req *parser.ParseRequest) (*parser.ParseResponse, error) {
 
-	log.Printf("Received parse request with format: %s", req.Format)
+	data, _ := server.GetData(req.GetRawData(), req.Format)
+	if len(data) == 0 {
+		errMsg := "No data received"
+		log.Print(errMsg)
+		return &parser.ParseResponse{
+			Success: false,
+			ErrMsg:  &errMsg,
+		}, nil
+	}
+	metarParser := goMetarParser.New()
+	count := 0
+	allReports := make([]*metar.Report, 0)
+	for _, fullDataString := range data {
+		metarDataSplitNewline := strings.Split(fullDataString, "\n")
+		for _, metarLine := range metarDataSplitNewline {
+			metarLine = strings.TrimSpace(metarLine)
+			if len(metarLine) == 0 || metarLine == "magic" {
+				continue
+			}
+
+			report, err := metarParser.Parse(metarLine)
+
+			if err != nil {
+				errMsg := "Error parsing METAR data: " + err.Error()
+				log.Print(errMsg)
+				return &parser.ParseResponse{
+					Success: false,
+					ErrMsg:  &errMsg,
+				}, nil
+			}
+
+			allReports = append(allReports, &report)
+			count++
+		}
+	}
+
+	jsonData, err := json.MarshalIndent(allReports, "", "  ")
+	if err != nil {
+		errMsg := "Error converting reports to JSON: " + err.Error()
+		log.Print(errMsg)
+	}
+
+	outputPath := "output.txt"
+	err = os.WriteFile(outputPath, jsonData, 0644)
+	if err != nil {
+		log.Printf("Error writing output file: %v", err)
+	}
+	log.Printf("Saved %d METAR reports to %s", count, outputPath)
 
 	return &parser.ParseResponse{
 		Success: true,
@@ -31,10 +83,3 @@ func main() {
 
 	log.Fatal(srv.Start())
 }
-
-//// https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKCH
-//func main() {
-//	p := parser.New()
-//	report, _ := p.Parse("EKCH 061620Z 32009KT 290V360 9999 OVC082/// 09/M04 Q1025 NOSIG=")
-//	fmt.Println(report)
-//}
