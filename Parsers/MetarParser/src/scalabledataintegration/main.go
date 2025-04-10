80041702743efb3427f959119c1cf728b326f722
Jonas Kaad <jonaskaad@gmail.com>
2025-04-10 12:45:02 +0200

    import of parser and filter code in go

--- Parsers/MetarParser/src/scalabledataintegration/main.go
+++ Parsers/MetarParser/src/scalabledataintegration/main.go
@@ -2,13 +2,45 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/eugecm/gometar/metar/parser"
+	filterPb "github.com/JonasKaad/ScalableDataIntegration/GeneratedClients/go/filter"
+	parserPb "github.com/JonasKaad/ScalableDataIntegration/GeneratedClients/go/parser"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
-// https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKCH
-func main() {
-	p := parser.New()
-	report, _ := p.Parse("EKCH 061620Z 32009KT 290V360 9999 OVC082/// 09/M04 Q1025 NOSIG=")
-	fmt.Println(report)
+type metarParserServer struct {
+	parserPb.UnimplementedParserServer
+	parser *parserPb.ParseRequest
 }
+
+type filterServerImpl struct {
+	filterPb.UnimplementedFilterServer
+	filter *filterPb.FilterRequest
+}
+
+func main() {
+	// Create a gRPC server
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	// Register your service implementation
+	parserPb.RegisterParserServer(s, &metarParserServer{})
+	filterPb.RegisterFilterServer(s, &filterServerImpl{})
+
+	fmt.Println("gRPC server listening on :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+//// https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKCH
+//func main() {
+//	p := parser.New()
+//	report, _ := p.Parse("EKCH 061620Z 32009KT 290V360 9999 OVC082/// 09/M04 Q1025 NOSIG=")
+//	fmt.Println(report)
+//}
