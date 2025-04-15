5e05697ed158d66ca25139d5ac53ae497202f7b5
Jonas Kaad <jonaskaad@gmail.com>
2025-04-15 13:02:12 +0200

    Updated CredentialsDialog layout

--- SkyPanel/Components/Dialogs/CredentialsDialog.razor.cs
+++ SkyPanel/Components/Dialogs/CredentialsDialog.razor.cs
@@ -1,3 +1,4 @@
+using CommonDis.Models;
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 
@@ -6,17 +7,25 @@ namespace SkyPanel.Components.Dialogs;
 public partial class CredentialsDialog : ComponentBase
 {
     [Parameter]
-    public  string Username { get; set; }= string.Empty;
+    public  string TokenName { get; set; }= string.Empty;
     
     [Parameter]
-    public string Password { get; set; } = string.Empty;
+    public string Token { get; set; } = string.Empty;
     
+    [Parameter]
+    public string SecretName { get; set; } = string.Empty;
     
     [CascadingParameter]
     private IMudDialogInstance? MudDialog { get; set; }
+
     private void DialogSubmit()
     {
-        MudDialog?.Close(DialogResult.Ok(true));
+        DisSecret newSecret = new DisSecret
+        {
+            TokenName = TokenName,
+            Token = Token
+        };
+        MudDialog?.Close(DialogResult.Ok((newSecret)));
     }
 
     private void DialogCancel() => MudDialog?.Cancel();
