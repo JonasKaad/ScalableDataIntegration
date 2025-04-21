f86eb3643d07546aa5c8e3e243ee47d44b1abffe
Jonas Kaad <jonaskaad@gmail.com>
2025-04-21 12:27:33 +0200

    Improved create-downloaders.sh script

--- create-downloaders.sh
+++ create-downloaders.sh
@@ -1,63 +1,81 @@
-curl -X POST "http://localhost:8080/Downloader/Add?downloader=metadataparser" \
-  -H "Content-Type: application/json" \
-  -d '{
-  "downloadUrl": "http://sdihttp.jonaskaad.com/chart.png",
-  "backUpUrl": "",
-  "parser": "metadataparser",
-  "filters": [
-    {
-      "name": "ocrfilter",
-      "parameters": {
-        "startX": "21.683421280821783",
-        "startY": "15.751422076198782",
-        "endX": "279.130063315068483",
-        "endY": "725.043190946062082"
-      }
-    },
-    {
-      "name": "regexfilter",
-      "parameters": {
-        "regex": "(VALID|valid)"
-      }
-    }
-  ],
-  "name": "metadataparser",
-  "pollingRate": "0 */22 * * *",
-  "secretName": "testing"
+#!/bin/bash
+
+# Base URL for downloadorchestrator
+BASE_URL="http://localhost:8080/Downloader/Add"
+
+add_downloader() {
+    local downloader=$1
+    local data=$2
+    
+    echo "Adding downloader: $downloader"
+    response=$(curl -s -w "%{http_code}" -X POST "${BASE_URL}?downloader=${downloader}" \
+        -H "Content-Type: application/json" \
+        -d "$data")
+    
+    if [ "${response: -3}" != "200" ]; then
+        echo "Error adding $downloader downloader"
+        exit 1
+    fi
+    echo "Successfully added $downloader"
+    echo "-------------------"
+}
+
+# Metadata parser - Processes image data with OCR and regex filtering
+add_downloader "metadataparser" '{
+    "downloadUrl": "http://sdihttp.jonaskaad.com/chart.png",
+    "backUpUrl": "",
+    "parser": "metadataparser",
+    "filters": [
+        {
+            "name": "ocrfilter",
+            "parameters": {
+                "startX": "21.683421280821783",
+                "startY": "15.751422076198782",
+                "endX": "279.130063315068483",
+                "endY": "725.043190946062082"
+            }
+        },
+        {
+            "name": "regexfilter",
+            "parameters": {
+                "regex": "(VALID|valid)"
+            }
+        }
+    ],
+    "name": "metadataparser",
+    "pollingRate": "0 */22 * * *",
+    "secretName": "testing"
 }'
 
-curl -X POST "http://localhost:8080/Downloader/Add?downloader=pythonparser" \
-  -H "Content-Type: application/json" \
-  -d '{
-  "downloadUrl": "https://api.met.no/weatherapi/tafmetar/1.0/taf.txt?icao=EKCH",
-  "backUpUrl": "",
-  "parser": "pythonparser",
-  "filters": [],
-  "name": "pythonparser",
-  "pollingRate": "0 */8 * * *",
-  "secretName": ""
+# Python parser - Processes TAF data from met.no
+add_downloader "pythonparser" '{
+    "downloadUrl": "https://api.met.no/weatherapi/tafmetar/1.0/taf.txt?icao=EKCH",
+    "backUpUrl": "",
+    "parser": "pythonparser",
+    "filters": [],
+    "name": "pythonparser",
+    "pollingRate": "0 */8 * * *",
+    "secretName": ""
 }'
 
-curl -X POST "http://localhost:8080/Downloader/Add?downloader=metarparser" \
-  -H "Content-Type: application/json" \
-  -d '{
-  "downloadUrl": "https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKOD",
-  "backUpUrl": "",
-  "parser": "metarparser",
-  "filters": [],
-  "name": "metarparser",
-  "pollingRate": "0 */11 * * *",
-  "secretName": "testing"
+# METAR parser - Processes METAR data from met.no
+add_downloader "metarparser" '{
+    "downloadUrl": "https://api.met.no/weatherapi/tafmetar/1.0/metar.txt?icao=EKOD",
+    "backUpUrl": "",
+    "parser": "metarparser",
+    "filters": [],
+    "name": "metarparser",
+    "pollingRate": "0 */11 * * *",
+    "secretName": "testing"
 }'
 
-curl -X POST "http://localhost:8080/Downloader/Add?downloader=ausotparser" \
-  -H "Content-Type: application/json" \
-  -d '{
-  "downloadUrl": "https://www.airservicesaustralia.com/flextracks/text.asp?ver=1",
-  "backUpUrl": "",
-  "parser": "ausotparser",
-  "filters": [],
-  "name": "ausotparser",
-  "pollingRate": "0 */6 * * *",
-  "secretName": ""
+# AUSOT parser - Processes Australian flight track data
+add_downloader "ausotparser" '{
+    "downloadUrl": "https://www.airservicesaustralia.com/flextracks/text.asp?ver=1",
+    "backUpUrl": "",
+    "parser": "ausotparser",
+    "filters": [],
+    "name": "ausotparser",
+    "pollingRate": "0 */6 * * *",
+    "secretName": ""
 }'
