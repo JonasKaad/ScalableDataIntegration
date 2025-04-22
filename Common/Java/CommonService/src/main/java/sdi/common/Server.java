55435c5f35dfd2a49a888be469ae6fe5965d4189
Jonas Kaad <jonaskaad@gmail.com>
2025-04-22 14:41:36 +0200

    Implemented basic gRPC server in common

--- Common/Java/CommonService/src/main/java/sdi/common/Server.java
+++ Common/Java/CommonService/src/main/java/sdi/common/Server.java
@@ -1,4 +1,35 @@
-package sdi.common;
+package src.main.java.sdi.common;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import sdi.parser.ParserGrpc;
+import sdi.parser.ParserOuterClass;
+
+import java.io.IOException;
 
 public class Server {
+
+    io.grpc.Server server;
+    private int port;
+
+    public Server(int port) {
+        this.port = port; // Default port
+    }
+
+    public void registerService(io.grpc.BindableService service) {
+        // Create a gRPC server on port 50051
+        server = ServerBuilder.forPort(port)
+                .addService(service)
+                .build();
+    }
+
+    public void startServer() throws IOException, InterruptedException {
+        // Start the server
+        server.start();
+        System.out.println("Parser Server started on port " + port);
+
+        // Keep the server running until terminated
+        server.awaitTermination();
+    }
 }
