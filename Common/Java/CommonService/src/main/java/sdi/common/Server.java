f1cbad6a5fab8fa27e9ef1db5b898a6e0ca5827f
Jonas Kaad <jonaskaad@gmail.com>
2025-04-23 10:10:02 +0200

    Implemented registering a service on the server

--- Common/Java/CommonService/src/main/java/sdi/common/Server.java
+++ Common/Java/CommonService/src/main/java/sdi/common/Server.java
@@ -1,15 +1,20 @@
 package src.main.java.sdi.common;
 
-import com.google.protobuf.ByteString;
+import io.github.cdimascio.dotenv.Dotenv;
 import io.grpc.ServerBuilder;
-import io.grpc.stub.StreamObserver;
-import sdi.parser.ParserGrpc;
-import sdi.parser.ParserOuterClass;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class Server {
 
+    private int retryInterval = 1000; // Retry interval in milliseconds
     io.grpc.Server server;
     private int port;
 
@@ -17,14 +22,14 @@ public class Server {
         this.port = port; // Default port
     }
 
-    public void registerService(io.grpc.BindableService service) {
+    public void registerServer(io.grpc.BindableService service) {
         // Create a gRPC server on port 50051
         server = ServerBuilder.forPort(port)
                 .addService(service)
                 .build();
     }
 
-    public void startServer() throws IOException, InterruptedException {
+    public void start() throws IOException, InterruptedException {
         // Start the server
         server.start();
         System.out.println("Parser Server started on port " + port);
@@ -32,19 +37,60 @@ public class Server {
         // Keep the server running until terminated
         server.awaitTermination();
     }
+
+    public boolean registerService() {
+        return registerService(Optional.empty());
+    }
+
+    public boolean registerService(Optional<String> registerType) {
+        String type = registerType.orElse("Parser");
+        Dotenv dotenv = Dotenv.load();
+        String parserName = dotenv.get("PARSER_NAME");
+        String parserUrl = dotenv.get("PARSER_URL");
+        String baseUrl = dotenv.get("BASE_URL");
+
+        String url = String.format("%s/%s/%s/register", baseUrl, type, parserName);
+
+        //send post request to url
+        try ( HttpClient client = HttpClient.newHttpClient() ) {
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString("{\"url\": \"" + parserUrl + "\"}"))
+                    .build();
+            try {
+                CompletableFuture<HttpResponse<String>> futureResponse = client.sendAsync(request,
+                        HttpResponse.BodyHandlers.ofString());
+                HttpResponse<String> response = futureResponse.get();
+                if (response.statusCode() == 200) {
+                    System.out.printf("Successfully registered %s of type: %s", parserName, type);
+                    return true;
+                } else {
+                    System.out.printf("Failed to register service: %s. %s", parserName, response.body());
+                    return false;
+                }
+            } catch (InterruptedException | ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public void initialize(String registerType) {
+        registerService(Optional.of(registerType));
+    }
+
     public void initialize() {
         while(true){
             boolean registered = registerService();
             if (registered) {
-                System.out.println("Service registered successfully.");
+                System.out.println("Registering successful. Starting heartbeat service...");
                 new Thread(() -> {
                     try {
-                        // Start the heartbeat service
                         new HeartbeatService().run();
                     } catch (Exception e) {
                         System.err.println("Error starting server: " + e.getMessage());
                     }
-                });
+                }).start();
                 break;
             } else {
                 System.out.println("Failed to register service. Retrying in " + retryInterval/1000 + " seconds...");
